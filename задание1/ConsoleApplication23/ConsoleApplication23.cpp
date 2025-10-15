94112864c26b7aa473742600f6ecc33c75b05910
Moy test <antonovdimamid@gmail.com>
2025-10-15 18:57:51 +0300

    commit2

---
+++ задание1/ConsoleApplication23/ConsoleApplication23.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+
+class Calculator
+{
+private:
+    double num1, num2;
+
+public:
+    double add()
+    {
+        return num1 + num2;
+    }
+    double multiply()
+    {
+        return num1 * num2;
+    }
+    double subtract_1_2()
+    {
+        return num1 - num2;
+    }
+    double subtract_2_1()
+    {
+        return num2 - num1;
+    }
+    double divide_1_2()
+    {
+        return num1 / num2;
+    }
+    double divide_2_1()
+    {
+        return num2 / num1;
+    }
+    bool set_num1(double num1)
+    {
+        if (num1 != 0) {
+            this->num1 = num1;
+            return true;
+        }
+        return false;
+    }
+    bool set_num2(double num2) {
+        if (num2 != 0) {
+            this->num2 = num2;
+            return true;
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double num1, num2;
+    int num3;
+
+    do {
+        Calculator good;
+
+
+        do {
+            std::cout << "Введите первое число, num1: ";
+            std::cin >> num1;
+            if (!good.set_num1(num1)) {
+                std::cout << "Неверный ввод! Число не должно быть равно 0." << std::endl;
+            }
+        } while (!good.set_num1(num1));
+
+
+        do {
+            std::cout << "Введите второе число, num2: ";
+            std::cin >> num2;
+            if (!good.set_num2(num2)) {
+                std::cout << "Неверный ввод! Число не должно быть равно 0." << std::endl;
+            }
+        } while (!good.set_num2(num2));
+
+
+        std::cout << "num1 + num2: " << good.add() << std::endl;
+        std::cout << "num1 * num2: " << good.multiply() << std::endl;
+        std::cout << "num1 - num2: " << good.subtract_1_2() << std::endl;
+        std::cout << "num2 - num1: " << good.subtract_2_1() << std::endl;
+        std::cout << "num1 / num2: " << good.divide_1_2() << std::endl;
+        std::cout << "num2 / num1: " << good.divide_2_1() << std::endl;
+
+        std::cout << "\nДля продолжения напишите 1, для окончания напишите 0\n" << std::endl;
+        std::cin >> num3;
+
+    } while (num3 != 0);
+
+    return 0;
+}
--- задание2/ConsoleApplication20/ConsoleApplication20.cpp
+++ задание2/ConsoleApplication20/ConsoleApplication20.cpp
@@ -2,61 +2,78 @@
 
 class Counter
 {
-
-	int count=0;
+    int count = 0;
 public:
-	void plus(int num )
-	{
-		this->count += num;
-	}
-	void set_count(int counter)
-	{
-		this->count = counter;
-	}
-	void result()
-	{
-		std::cout << count;
-	}
+    void plus()
+    {
+        count++;
+    }
+    void minus()
+    {
+        count--;
+    }
+    int result()
+    {
+        return count;
+    }
+    void setter_count(int counter)
+    {
+        this->count = counter;
+    }
 };
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	Counter counter;
-		char symbol;
-		int num1;
-		int start_count;
-		std::cout << "Вы хотите поставить начальное число счетчика?" << std::endl;
-		std::cout << "Если да, то напишите число с которого начнет работать счетчик, если нет - напишите 0" << std::endl;
-		std::cin >> start_count;
-		if (start_count > 0)
-		{
-			num1 = start_count;
-		}
-		else {
-			num1 = 0;
-		}
-		std::cout << "Значение счетчика: " << num1 << std::endl;
+    setlocale(LC_ALL, "Russian");
+    Counter counter;
+    int num1;
+    char symbol;
+    int start_count;
 
-		do{
-			std::cout << "\nВведите команду ( + , - ,  =  или  x ):" << std::endl;
-			std::cin >> symbol;
-			if (symbol == '+')
-			{
-				num1++;
-				counter.set_count(num1);
-			}
-			if (symbol == '-')
-			{
-				num1--;
-				counter.set_count(num1);
-			}
+    std::cout << "Вы хотите поставить начальное число счетчика?" << std::endl;
+    std::cout << "Если да, то напишите число с которого начнет работать счетчик, если нет - напишите 0" << std::endl;
+    std::cin >> num1;
 
-			if (symbol == '=')
-			{
-				std::cout << "Результат счетчика: "; counter.result();
-			}
+    if (num1 > 0)
+    {
+        start_count = num1;
+    }
+    if (num1 == 0)
+    {
+        start_count = 1;
+    }
+    counter.setter_count(start_count);
 
+    std::cout << "Значение счетчика: " << counter.result() << std::endl;
 
-		} while (symbol != 'x');
+    do {
+        std::cout << "\nВведите команду ( + , - , = или x ):" << std::endl;
+        std::cin >> symbol;
+
+        if (symbol == '+')
+        {
+            counter.plus();
+        }
+        else if (symbol == '-')
+        {
+            counter.minus();
+        }
+        else if (symbol == '=')
+        {
+            std::cout << "Результат счетчика: " << counter.result() << std::endl;
+        }
+        else if (symbol == 'x')
+        {
+            std::cout << "Завершение работы программы." << std::endl;
+        }
+        if (symbol != 'x' && symbol != '+' && symbol != '-' && symbol != '=')
+        {
+            std::cout << "\nОшибка! Недопустимая команда. Используйте только: +, -, = или x" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(10000, '\n');
+        }
+
+    } while (symbol != 'x');
+
+    return 0;
 }
